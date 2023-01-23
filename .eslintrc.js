becb91b2cf2e88b7ea15930735e1b9ac6e50a99d
ezekielalene <ezkielmisgae@gmail.com>
2023-01-24 01:46:51 +0300

    First task

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,77 @@
-<<<<<<< HEAD
 
-rts = {
-	  env: {
-		      browser: false,
-		      es6: true,
-		      jest: true,
-		    },
-	  extends: [
-		      'airbnb-base',
-		      'plugin:jest/all',
-		    ],
-	  globals: {
-		      Atomics: 'readonly',
-		      SharedArrayBuffer: 'readonly',
-		    },
-	  parserOptions: {
-		      ecmaVersion: 2018,
-		      sourceType: 'module',
-		    },
-	  plugins: ['jest'],
-	  rules: {
-		      'no-console': 'off',
-		      'no-shadow': 'off',
-		      'no-restricted-syntax': [
-			            'error',
-			            'LabeledStatement',
-			            'WithStatement',
-			          ],
-		    },
-	  overrides:[
-		      {
-			            files: ['*.js'],
-			            excludedFiles: 'babel.config.js',
-			          }
-		    ]
+module.exports = {
+    env: {
+        browser: false,
+        es6: true,
+        jest: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:jest/all',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
+    ]
 };
 
-=======
+rts = {
+    env: {
+        browser: false,
+        es6: true,
+        jest: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:jest/all',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+        'no-console': 'off',
+        'no-shadow': 'off',
+        'no-restricted-syntax': [
+            'error',
+            'LabeledStatement',
+            'WithStatement',
+        ],
+    },
+    overrides: [
+        {
+            files: ['*.js'],
+            excludedFiles: 'babel.config.js',
+        }
+    ]
+};
+
+
 module.exports = {
     env: {
       browser: false,
@@ -72,4 +107,3 @@ module.exports = {
       }
     ]
   };
->>>>>>> 22229ecce60e7dce3c4eacd050a5608c59c38619
